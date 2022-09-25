8e289bed422e6de472cc65b4dc5a8261176a0172
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-25 15:10:02 +1000

    completed challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener("DOMContentLoaded", doWork);
+
+function doWork(){
+    getDogImages();
+}
+
+//Challenge 1
+function getDogImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    let container = document.querySelector("#dog-image-container");
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then(data => {
+            data.message.forEach((dogUrl => {
+                container.innerHTML += `<img src="${dogUrl}"/>`
+            }))
+        });
+}
+
